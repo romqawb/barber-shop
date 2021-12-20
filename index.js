d560f37be14c48cdaab0edc4307b5316b4c2ab56
Roman Sukys <roman.sukys@gmail.com>
2021-12-20 10:43:42 +0000

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+var app = express();
+
+
+//something changed
+
+
+
+app.get('/', (req, res) => {
+    res.send('hello');
+})
+
+app.listen(3000, () => {
+    console.log('up');
+})
