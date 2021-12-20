905d6d3c156c0d888db8a6effaa6f9800ffb5d9b
Roman <roman.sukys@gmail.com>
2021-12-20 11:58:04 +0000

    home page, stylesheet and packages added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 //APP INIT
 import express from 'express';
 import path from 'path';
+const __dirname = path.resolve();
 const app = express();
 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('hello');
+    res.render('home');
 })
 
 app.listen(3000, () => {
